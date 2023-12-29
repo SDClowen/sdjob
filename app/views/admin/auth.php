1793beb236dc68e85ea903b1b1024f135e60a084
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-12-29 11:03:10 +0300

    ...

--- app/views/admin/auth.php
+++ app/views/admin/auth.php
@@ -50,7 +50,7 @@
                                         <label for="remember" class="text-gray-500 dark:text-gray-300">Beni Hatırla</label>
                                     </div>
                                 </div>
-                                <a href="#" class="text-sm font-medium text-red-600 hover:underline dark:text-red-500">Şifremi Unuttum</a>
+                                <a href="#" class="hidden text-sm font-medium text-red-600 hover:underline dark:text-red-500">Şifremi Unuttum</a>
                             </div>
                             <button type="submit" class="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Giriş Yap</button>
                         </form>
